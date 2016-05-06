a287ea293949a35a896bbc2d8106fedf88ee51d7
Gao Shengxu <1059067158@qq.com>
2016-05-06 18:01:47 +0800

    Learning...

--- Sql/PHP_mysql/Project/login.php
+++ Sql/PHP_mysql/Project/login.php
@@ -1,7 +1,32 @@
 <?php
 
-	//用户登录 
+	//用户登录 ：负责处理 与登录 相关的所有请求（加载表单 ，验证登录）
 	//加载登录表单
 
 
-	include_once'templates/login.html';
+	//应该先判断 用户 意图：如果 提交数据 为了验证
+	if(isset($_POST['submit']))
+	{
+	 //验证用户信息
+	
+	 //接收表单数据
+	 $username=$_POST['username'];
+	 $password=$_POST['password'];
+
+		if(empty($username)||empty($password))
+		{
+			//用户名 或者密码为空
+			//跳转
+
+			header('Refresh:3;url=www.baidu.com');
+			echo '用户名和密码都不能为空！';
+
+		}
+	}
+	else
+	{
+
+	//加载登录表单
+	include_once'templates/login.html';	
+	}
+
